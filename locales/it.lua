6032060ec0991211ce5ae8393fa2bba76c660885
Dev Capy <78591839+mr-capy@users.noreply.github.com>
2022-07-02 19:09:25 +0500

    Create it.lua

---
+++ locales/it.lua
@@ -0,0 +1,23 @@
+Locales['it'] = {
+    --Menus 
+    ['main_Menu_title'] = 'Menu modifica identità',
+    ['press_menu'] = 'premi [E] per cambiare il tuo nome.',
+    ['current_name_menu_title'] = 'Nome corrente',
+    ['get_firstname_menu'] = 'Prendi il primo nome',
+    ['get_secondname_menu'] = 'Prendi il secondo nome',
+    ['show_currentname_label'] = 'Mostra nome corrente',
+    ['set_newname_label'] = 'Imposta nuovo nome',
+
+    --Blip
+    ['namechanger'] = 'Ufficio cambio identità',
+
+    --Notifications
+    ['new_name_updated_success'] = 'Nuovo nome aggiornato correttamente',
+    ['new_name_is'] = 'Il tuo nome è %s %s',
+    ['database_error_contact_admin'] = 'Aggiornamento nome fallito, contatta lo sviluppatore.',
+    ['reuired_money'] = 'Hai bisogno di $%s',
+    ['firstname_error'] = 'Il nuovo nome deve avere più di 2 lettere',
+    ['lastname_error'] = 'Il nuovo cognome deve avere più di 2 lettere',
+}
+
+--by : https://forum.cfx.re/u/ofcshiro/
