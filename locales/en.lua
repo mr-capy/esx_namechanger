f2e273a0cfcd25b35cbc515519b33bd2084f6917
Dev Capy <78591839+mr-capy@users.noreply.github.com>
2022-07-02 18:33:43 +0500

    en.lua

---
+++ locales/en.lua
@@ -0,0 +1,21 @@
+Locales['en'] = {
+   --Menus 
+    ['main_Menu_title'] = 'Edit Identity Menu',
+    ['press_menu'] = 'press [E] to Change your name.',
+    ['current_name_menu_title'] = 'Current Name',
+    ['get_firstname_menu'] = 'Get First Name',
+    ['get_secondname_menu'] = 'Get Second Name',
+    ['show_currentname_label'] = 'Show Current Name',
+    ['set_newname_label'] = 'Set New Name',
+
+    --Blip
+    ['namechanger'] = 'Identity Change Office',
+
+    --Notifications
+    ['new_name_updated_success'] = 'New Name Updated Successfully',
+    ['new_name_is'] = 'Your Name is %s %s',
+    ['database_error_contact_admin'] = 'Name Updation Failed Contact Developer.',
+    ['reuired_money'] = 'You need $%s',
+    ['firstname_error'] = 'New First Name must have more than 2 letters',
+    ['lastname_error'] = 'New Last Name must have more than 2 letters',
+    }
