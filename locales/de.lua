fc956808b23dc6f9049f495e257d84464cdcc831
Dev Capy <78591839+mr-capy@users.noreply.github.com>
2022-07-02 19:10:17 +0500

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,25 @@
+Locales['de'] = {
+   --Menus 
+    ['main_Menu_title'] = 'Namensänderung',
+    ['press_menu'] = 'Drücke [E] um deinen Namen zu ändern.',
+    ['current_name_menu_title'] = 'Aktueller Name',
+    ['get_firstname_menu'] = 'Vorname',
+    ['get_secondname_menu'] = 'Nachname',
+    ['show_currentname_label'] = 'Aktuellen Namen anzeigen',
+    ['set_newname_label'] = 'Aktuellen Namen ändern',
+
+    --Blip
+    ['namechanger'] = 'Einwohner Meldeamt',
+
+    --Notifications
+    ['new_name_updated_success'] = 'Dein Name wurde erfolgreich geändert',
+    ['new_name_is'] = 'Dein neuer Name lautet %s %s',
+    ['database_error_contact_admin'] = 'Namensänderung fehlgeschlagen, Kontaktiere einen Admin.',
+    ['reuired_money'] = 'You need $%s',
+    ['firstname_error'] = 'Dein Vorname muss mehr als 2 Zeichen haben',
+    ['lastname_error'] = 'Dein Nachname muss mehr als 2 Zeichen haben',
+    
+
+}
+
+--by -- https://forum.cfx.re/u/ofcshiro/
